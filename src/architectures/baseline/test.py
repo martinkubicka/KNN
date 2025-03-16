20daddeae2d255ff3fc5df466a826db1e624b579
martinkubicka <martinkubicka22@gmail.com>
2025-03-16 20:35:54 +0100

    Implement get_representative_embedding function in testing loop

--- src/architectures/baseline/test.py
+++ src/architectures/baseline/test.py
@@ -3,6 +3,7 @@ import torch.utils.data as data
 from dataset import HWDataset
 from globals import TRANSFORM, DEVICE, MODEL_PATH, IMG_SIZE, BATCH_SIZE, DATA_MDB_PATH, TEST_CSV_PATH
 from model import create_model
+from collections import defaultdict
 
 def get_class(embedding, class_to_embedding):
     max_sim, best_class = -float("inf"), None
@@ -13,18 +14,31 @@ def get_class(embedding, class_to_embedding):
             best_class = class_name
     return best_class
 
-# TODO - Create function which will get representative embeddings 
-#        from test dataset and ignore this representative data.
-#        Example of format below.
-# class_to_embedding = {
-#     "0": torch.tensor([1]).to(DEVICE),
-#     "1": torch.tensor([2]).to(DEVICE),
-# }
-def get_representative_embeddings():
-    return  {
-                "0": torch.tensor([1]).to(DEVICE),
-                "1": torch.tensor([2]).to(DEVICE),
-            }
+def get_representative_embeddings(model, test_loader, num_samples_per_class=10):
+    class_to_embeddings = defaultdict(list)
+
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images = images.to(DEVICE)
+            labels = labels.to(DEVICE)
+
+            embeddings = model(images)
+
+            for emb, label in zip(embeddings, labels):
+                label_str = str(label.item())
+
+                if len(class_to_embeddings[label_str]) < num_samples_per_class:
+                    class_to_embeddings[label_str].append(emb)
+
+            if all(len(v) >= num_samples_per_class for v in class_to_embeddings.values()):
+                break
+
+    class_to_embedding = {
+        label: torch.stack(emb_list).mean(dim=0).to(DEVICE)
+        for label, emb_list in class_to_embeddings.items()
+    }
+
+    return class_to_embedding
 
 test_dataset = HWDataset(DATA_MDB_PATH, TEST_CSV_PATH,transform=TRANSFORM)
 test_loader = data.DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
@@ -33,7 +47,7 @@ model = create_model(num_classes=None, img_size=IMG_SIZE)
 model.load_state_dict(torch.load(MODEL_PATH))
 model.eval()
 
-class_to_embedding = get_representative_embeddings()
+class_to_embedding = get_representative_embeddings(model, test_loader)
 
 correct, total = 0, 0
 with torch.no_grad():
